f35cb38ba16acdddcc7e7c6e7d5d47a58a7edb89
meepstertron <62438310+meepstertron@users.noreply.github.com>
2023-07-04 13:13:34 +0200

    5

--- chat.js
+++ chat.js
@@ -2,81 +2,32 @@ const urlParams = new URLSearchParams(window.location.search);
 const key = urlParams.get('key');
 console.log(key);
 
-
-
-function Send() {
-
-    var selLang = "en-US"
-
-    var sQuestion = txtMsg.value;
-    if (sQuestion == "") {
-        alert("Type in your question!");
-        txtMsg.focus();
-        return;
-    }
-
-    var oHttp = new XMLHttpRequest();
-    oHttp.open("POST", "https://api.openai.com/v1/completions");
-    oHttp.setRequestHeader("Accept", "application/json");
-    oHttp.setRequestHeader("Content-Type", "application/json");
-    oHttp.setRequestHeader("Authorization", "Bearer " + key)
-
-    oHttp.onreadystatechange = function () {
-        if (oHttp.readyState === 4) {
-            //console.log(oHttp.status);
-            var oJson = {}
-            if (txtOutput.value != "") txtOutput.value += "\n";
-
-            try {
-                oJson = JSON.parse(oHttp.responseText);
-            } catch (ex) {
-                txtOutput.value += "Error: " + ex.message
-            }
-
-            if (oJson.error && oJson.error.message) {
-                txtOutput.value += "Error: " + oJson.error.message;
-            } else if (oJson.choices && oJson.choices[0].text) {
-                var s = oJson.choices[0].text;
-
-                if (selLang.value != "en-US") {
-                    var a = s.split("?\n");
-                    if (a.length == 2) {
-                        s = a[1];
-                    }
-                }
-
-                if (s == "") s = "No response";
-                txtOutput.value += "Chat GPT: " + s;
-                
-            }            
-        }
-    };
-
-    var sModel = "text-davinci-003";
-    var iMaxTokens = 4096;
-    var sUserId = "1";
-    var dTemperature = 0.5;    
-
-    var data = {
-        model: sModel,
-        prompt: sQuestion,
-        max_tokens: iMaxTokens,
-        user: sUserId,
-        temperature:  dTemperature,
-        frequency_penalty: 0.0, //Number between -2.0 and 2.0  
-                                //Positive values decrease the model's likelihood 
-                                //to repeat the same line verbatim.
-        presence_penalty: 0.0,  //Number between -2.0 and 2.0. 
-                                //Positive values increase the model's likelihood 
-                                //to talk about new topics.
-        stop: ["#", ";"]        //Up to 4 sequences where the API will stop 
-                                //generating further tokens. The returned text 
-                                //will not contain the stop sequence.
-    }
-
-    oHttp.send(JSON.stringify(data));
-
-    if (txtOutput.value != "") txtOutput.value += "\n";
-    txtOutput.value += "Me: " + sQuestion;
-    txtMsg.value = "";
-}
+var send = function(textin){
+    fetch( `https://api.openai.com/v1/chat/completions`,
+    {
+      body: JSON.stringify({
+        “model”: “gpt-3.5-turbo”,
+          “messages”: [
+            {role: “system”, content: “You are the chatbot of a website called moleculARweb, which provides educational material for chemistry using commodity augmented reality. You answer questions about the website, about chemistry, science, etc.”},
+            {role: “user”, content: “What is the formula of acetic acid?”},
+            {role: “assistant”, content: “The formula of acetic acid is CH3COOH”},
+            {role: “user”, content: textin}
+          ],
+          “temperature”: 0.3,
+          “max_tokens”: 2000
+        }), 
+        method: “POST”,
+        headers: {
+        “content-type”: “application/json”,
+        Authorization: “Bearer “ + key,
+      }
+    }).then((response) => {
+      console.log(response) //If you want to check the full response
+      if (response.ok) {
+        response.json().then((json) => {
+          console.log(json);  //If you want to check the response as JSON
+          console.log(json.choices[0].message.content) //HERE'S THE CHATBOT'S RESPONSE
+        });
+      }
+    });
+   }
